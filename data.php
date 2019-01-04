3fe774456047875de3c8a93c33047d86b26c7017
jwhaeke <jwhaeke@live.nl>
2019-01-04 11:36:14 +0100

    Created an array with games, genre, price and review value

---
+++ data.php
@@ -0,0 +1,14 @@
+<?php
+
+$listGames = array (
+    array ('Diablo', 'RPG', 60, 4.5),
+    array ('Black Ops 3', 'FPS', 45, 4),
+    array ('Candy Crush', 'Arcade', 5, 3),
+    array ('Fantasy Story', 'RPG', 20, 2),
+    array ('H1Z1', 'FPS', 0, 3.5),
+    array ('Flappy Bird', 'Arcade', 2, 1)
+);
+
+
+
+?>
