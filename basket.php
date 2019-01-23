e64511760b93d4a4ccc8a26a6bf2a6a01633b723
jwhaeke <jwhaeke@live.nl>
2019-01-23 10:18:39 +0100

    Removed bug in basket php. Undeclared var

--- basket.php
+++ basket.php
@@ -18,6 +18,7 @@ try {
     $conn = new \PDO("mysql:host=127.0.0.1;dbname=platform", "root", "pannenkoek");
     $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     $_SESSION['basket'] = [];
+    $sum = 0;
     // Search for orders made by the user and echo them - Also use data from the games table
     $stmt = $conn->prepare("SELECT orders.game_id, orders.id, games.name, games.price FROM orders INNER JOIN games ON orders.game_id=games.id WHERE user_id = :user");
     $stmt->execute(['user' => $_SESSION['user_id']]);
